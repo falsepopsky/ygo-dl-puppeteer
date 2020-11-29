6e52832c152fac1ba69e52f8642ee8b73c96b0aa
falsepopsky <javicream@gmail.com>
2020-11-28 23:27:15 -0300

    pupeeteer working in ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+/* let nameBox = 'voltageofthemetal';
+let duelLinksBox = `https://www.konami.com/yugioh/duel_links/en/box/${nameBox}/`;
+
+async function start() {
+    const data = await openDuelLinksSite(duelLinksBox);
+    console.log(data);
+  }
+  
+  start();
+  
+ */
--- src/services/puppeteer/index.ts
+++ src/services/puppeteer/index.ts
@@ -1,36 +1,36 @@
 import puppeteer from 'puppeteer';
 
-const URL_DUEL_LINKS_BOX = 'https://www.konami.com/yugioh/duel_links/en/box/';
-
-/*  async function selectColumn4(param: string) {
-  const data = await document.querySelector(param);
-  if (!data) {
-    return undefined;
-  } else {
-    console.log(data.innerHTML);
-    return data.innerHTML;
-  }
+interface cardItem {
+  id: number;
+  name: string;
+  type: string;
 }
 
-*/
-
-async function puppeteerStart(url: string) {
+async function openDuelLinksSite(url: string) {
   const browser = await puppeteer.launch();
   try {
     const page = await browser.newPage();
     await page.goto(url);
 
     const result = await page.evaluate(() => {
-      const containerDuelLinksPosts = document.querySelector<HTMLInputElement>(
-        '.rpBJOHq2PR60pnwJlUyP0 div'
-      );
-      let temperatureText: any;
-      if (!containerDuelLinksPosts) {
-        console.log('no hay posts de Duel Links');
+      const container = document.querySelector<HTMLInputElement>('.card-list');
+      let objectCards: cardItem[] = [];
+
+      if (!container) {
+        console.log('no hay container');
       } else {
-        temperatureText = containerDuelLinksPosts.innerText;
-        return temperatureText;
+        let cards = container.querySelectorAll<HTMLInputElement>('li')!;
+
+        cards.forEach((card, index) => {
+          let id = index;
+          let name = card.querySelector<HTMLInputElement>('dt')!.innerText;
+          let type = card.querySelector<HTMLInputElement>('dd')!.innerText;
+          const dataCard = { id, name, type };
+          objectCards.push(dataCard);
+        });
       }
+
+      return objectCards;
     });
 
     return result;
@@ -41,10 +41,3 @@ async function puppeteerStart(url: string) {
     await browser.close();
   }
 }
-
-async function start() {
-  const data = await puppeteerStart(URL_DUEL_LINKS_BOX);
-  console.log(data);
-}
-
-start();
