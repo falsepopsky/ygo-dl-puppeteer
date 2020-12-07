efc80889a226278562701bad47102e55ea696174
falsepopsky <javicream@gmail.com>
2020-12-06 22:58:17 -0300

    test

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import { getCards, getInfo } from './src/services/puppeteer/index';
 let nameBox: string = 'voltageofthemetal';
 
 async function start() {
+  console.log('-------------');
+  console.log('Started');
   const cardsBoxData = await getCards(nameBox);
   const infoBoxData = await getInfo(nameBox);
 }
