6707c7231450f48e8b74585269ce654df9590f55
falsepopsky <javicream@gmail.com>
2020-12-08 23:21:56 -0300

    .env added

--- index.ts
+++ index.ts
@@ -3,8 +3,6 @@ import { getCards, getInfo } from './src/services/puppeteer/index';
 let nameBox: string = 'voltageofthemetal';
 
 async function start() {
-  console.log('-------------');
-  console.log('Started');
   const cardsBoxData = await getCards(nameBox);
   const infoBoxData = await getInfo(nameBox);
 }
